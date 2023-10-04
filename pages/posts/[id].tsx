498686279625849dfa6a0575fbc78820e2513e10
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-10-04 22:08:56 +0900

    feat: getStaticPaths
    import and show post data (remark)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,48 @@
+import { GetStaticPaths } from 'next';
 import React from 'react';
+import { getAllPostIds, getPostData } from '@/lib/post';
+import Head from 'next/head';
+import { homeStyles } from '@/styles/Home.module.css';
 
-const Post = () => {
-  return <div>[id]</div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering'}, {param...} }]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
